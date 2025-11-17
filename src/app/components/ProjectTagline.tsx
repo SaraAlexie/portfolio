fbe8925271e465a1146bc317a4851b0800436560
Sara Nederskov <sara.nederskov@hotmail.com>
2025-11-17 16:34:12 +0100

    color changes

--- src/app/components/ProjectTagline.tsx
+++ src/app/components/ProjectTagline.tsx
@@ -8,7 +8,7 @@ type ProjectInfoProps = {
 export default function ProjectTagline({ title, tagline }: ProjectInfoProps) {
     return (
         <>
-            <h3 className="text-secondary font-semibold text-base mb-1 line-clamp-1">
+            <h3 className="text-primary-text font-semibold text-base mb-1 line-clamp-1">
                 {title}
             </h3>
             <p className="text-primary-text text-xs line-clamp-2 flex-1">
