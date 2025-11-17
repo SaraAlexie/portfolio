e9461c44e40bbc2b35c8a2441fdf6f6d2261f849
Sara Nederskov <sara.nederskov@hotmail.com>
2025-11-17 09:13:42 +0100

    add tagline to data and projects

--- src/app/components/ProjectTagline.tsx
+++ src/app/components/ProjectTagline.tsx
@@ -2,20 +2,17 @@
 
 type ProjectInfoProps = {
     title: string;
-    description: string;
+    tagline?: string;
 };
 
-export default function ProjectDescription({
-    title,
-    description,
-}: ProjectInfoProps) {
+export default function ProjectTagline({ title, tagline }: ProjectInfoProps) {
     return (
         <>
             <h3 className="text-secondary font-semibold text-base mb-1 line-clamp-1">
                 {title}
             </h3>
-            <p className="text-primary-text text-xs line-clamp-3 flex-1">
-                {description}
+            <p className="text-primary-text text-xs line-clamp-2 flex-1">
+                {tagline ? tagline : "No tagline availiable"}
             </p>
         </>
     );
